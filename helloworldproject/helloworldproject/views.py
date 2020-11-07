3770212cac6c7ad528154b564def33ccdd89d757
Kazoo1122 <syayo181@gmail.com>
2020-11-07 21:15:10 +0900

    New app added

--- helloworldproject/helloworldproject/views.py
+++ helloworldproject/helloworldproject/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def helloworldfunc(request):
-    return HttpResponse('hello world')
+
+def hello_world_func(request):
+    response_object = HttpResponse('<h1>hello world</h1>')
+    return response_object
+
+
+class HelloWorldClass(TemplateView):
+    template_name = 'hello.html'
